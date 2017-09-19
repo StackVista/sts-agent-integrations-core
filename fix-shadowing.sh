e76166bc41d0d477e3c7d0c69155283f2e252404
Bram Schuur <bschuur@stackstate.com>
2017-09-19 11:07:05 +0200

    Make shadowing fix work on travis

--- fix-shadowing.sh
+++ fix-shadowing.sh
@@ -4,6 +4,10 @@ set -e
 
 echo "This scripts removes directories from the virtualenv to avoid clashes between directories the stackstate agent exposes and packages in the virtualenv"
 
-rm -rf venv/lib/python2.7/site-packages/tests
+set -x
+
+rm -rf $(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")/tests
+
+set +x
 
 echo "Done"

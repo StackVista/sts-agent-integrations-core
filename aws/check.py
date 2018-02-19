c7abefa2545dc9bf3b7a1ae247397ae4da362de2
Wytze Hazenberg <whazenberg@stackstate.com>
2018-02-19 16:45:24 +0100

    datetime json parsing fix and added health event

--- aws/check.py
+++ aws/check.py
@@ -4,6 +4,7 @@
 
 # stdlib
 import json
+import time
 
 # 3rd party
 
@@ -32,7 +33,7 @@ class AwsCheck(AgentCheck):
         self.start_snapshot(self.instance_key)
 
         self.process_elb_classic()
-        # self.process_elb_v2()
+        self.process_elb_v2()
 
         self.stop_snapshot(self.instance_key)
 
@@ -40,6 +41,7 @@ class AwsCheck(AgentCheck):
         # ELB classic
         for elb in self.client.describe_load_balancers()['LoadBalancerDescriptions']:
             external_id = "classic_elb_" + elb["LoadBalancerName"]
+            elb['CreatedTime'] = elb['CreatedTime'].isoformat()
             data = elb
 
             self.component(self.instance_key, external_id, {'name': 'elb_classic'}, data)
@@ -53,16 +55,15 @@ class AwsCheck(AgentCheck):
                         'Instance': instance,
                         'ListenerDescriptions': elb['ListenerDescriptions']
                     }
-
                     self.component(self.instance_key, instance_external_id, {'name': 'elb_classic_instance'}, instance_data)
-
                     self.relation(self.instance_key, external_id, instance_external_id, {'name': 'elb_classic_has_instance'}, {})
 
     def process_elb_v2(self):
         for elbv2 in self.clientv2.describe_load_balancers()['LoadBalancers']:
             elb_external_id = elbv2['LoadBalancerArn']
+            elbv2['CreatedTime'] = elbv2['CreatedTime'].isoformat()
             elb_data = elbv2
-            elb_type = "elb_v2_" + elbv2['Type']  # for example, elb_application
+            elb_type = "elb_v2_" + elbv2['Type'].lower()  # for example, elb_application
 
             # listeners
             elb_data['listeners'] = []
@@ -72,7 +73,7 @@ class AwsCheck(AgentCheck):
             self.component(self.instance_key, elb_external_id, {'name': elb_type}, elb_data)
 
         # target group
-        for target_group in self.clientv2.describe_target_groups(LoadBalancerArn=elbv2["LoadBalancerArn"])["TargetGroups"]:
+        for target_group in self.clientv2.describe_target_groups()["TargetGroups"]:
             target_group_external_id = target_group['TargetGroupArn']
             target_group_data = target_group
 
@@ -84,15 +85,23 @@ class AwsCheck(AgentCheck):
 
             # target
             for target in self.clientv2.describe_target_health(TargetGroupArn=target_group['TargetGroupArn'])['TargetHealthDescriptions']:
-                pprint.pprint(target)
-
-                # TODO health check information -> event
-
                 # assuming instance here, IP is another option
                 target_external_id = target['Target']['Id'] + ':' + str(target['Target']['Port'])
                 target_data = {}
-
                 self.component(self.instance_key, target_external_id, {'name': 'elb_v2_target_group_instance'}, target_data)
 
                 # relation between target group and target
                 self.relation(self.instance_key, target_group_external_id, target_external_id, {'name': 'elb_v2_is_target'}, {})
+
+                # health check event
+                event = {
+                    "timestamp": int(time.time()),
+                    "event_type": EVENT_TYPE + "_elb_health",
+                    "msg_title": target['Target']['Id'] + ":" + str(target['Target']['Port']) + " is " + target['TargetHealth']['State'],
+                    "msg_text": '' if target['TargetHealth']['State'] == 'healthy' else target['TargetHealth']['Description'] + '. Reason: ' + target['TargetHealth']['Reason'],
+                    "source_type_name": SOURCE_TYPE_NAME,
+                    "host": target_group_external_id,
+                    "tags": []
+                }
+
+                self.event(event)

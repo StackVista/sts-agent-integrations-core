42867e538b7a32d9a1fb9ab7a88b426fd3692184
Wytze Hazenberg <whazenberg@stackstate.com>
2019-01-28 18:54:50 +0100

    fix indentation

--- static_topology/check.py
+++ static_topology/check.py
@@ -11,87 +11,86 @@ from checks import AgentCheck, CheckException
 
 
 class StaticTopology(AgentCheck):
-  SERVICE_CHECK_NAME = "StaticTopology"
+    SERVICE_CHECK_NAME = "StaticTopology"
 
-  def check(self, instance):
-    if 'components_file' not in instance:
-        raise CheckException('Static topology instance missing "components_file" value.')
-    if 'type' not in instance:
-        raise CheckException('Static topology instance missing "type" value.')
+    def check(self, instance):
+        if 'components_file' not in instance:
+            raise CheckException('Static topology instance missing "components_file" value.')
+        if 'type' not in instance:
+            raise CheckException('Static topology instance missing "type" value.')
 
-    instance_tags = instance['tags'] if 'tags' in instance else []
+        instance_tags = instance['tags'] if 'tags' in instance else []
 
-    if instance['type'].lower() == "csv":
-      component_file = instance['components_file']
-      relation_file = instance['relations_file'] if 'relations_file' in instance else None
-      delimiter = instance['delimiter']
-      instance_key = {"type": "StaticTopology", "url": component_file}
-      self.handle_component_csv(instance_key, component_file, delimiter, instance_tags)
-      if relation_file:
-        self.handle_relation_csv(instance_key, relation_file, delimiter, instance_tags)
-    else:
-      raise CheckException('Static topology instance only supports type CSV.')
+        if instance['type'].lower() == "csv":
+            component_file = instance['components_file']
+            relation_file = instance['relations_file'] if 'relations_file' in instance else None
+            delimiter = instance['delimiter']
+            instance_key = {"type": "StaticTopology", "url": component_file}
+            self.handle_component_csv(instance_key, component_file, delimiter, instance_tags)
+            if relation_file:
+                self.handle_relation_csv(instance_key, relation_file, delimiter, instance_tags)
+        else:
+            raise CheckException('Static topology instance only supports type CSV.')
 
-  def handle_component_csv(self, instance_key, filelocation, delimiter, instance_tags):
-    self.log.debug("Processing component CSV file %s." % filelocation)
+    def handle_component_csv(self, instance_key, filelocation, delimiter, instance_tags):
+        self.log.debug("Processing component CSV file %s." % filelocation)
 
-    COMPONENT_ID_FIELD = 'id'
-    COMPONENT_TYPE_FIELD = 'type'
-    COMPONENT_NAME_FIELD = 'name'
+        COMPONENT_ID_FIELD = 'id'
+        COMPONENT_TYPE_FIELD = 'type'
+        COMPONENT_NAME_FIELD = 'name'
 
-    with codecs.open(filelocation, mode='r', encoding="utf-8-sig") as csvfile:
-      reader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
+        with codecs.open(filelocation, mode='r', encoding="utf-8-sig") as csvfile:
+            reader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
 
-      header_row = next(reader, None)
-      if header_row is None:
-        raise CheckException("Component CSV file is empty.")
-      self.log.debug("Detected component header: %s" % str(header_row))
+            header_row = next(reader, None)
+            if header_row is None:
+                raise CheckException("Component CSV file is empty.")
+            self.log.debug("Detected component header: %s" % str(header_row))
 
-      if len(header_row) == 1:
-        self.log.warn("Detected one field in header, is the delimiter set properly?")
-        self.log.warn("Detected component header: %s" % str(header_row))
+            if len(header_row) == 1:
+                self.log.warn("Detected one field in header, is the delimiter set properly?")
+                self.log.warn("Detected component header: %s" % str(header_row))
 
-      # mandatory fields
-      for field in (COMPONENT_ID_FIELD, COMPONENT_NAME_FIELD, COMPONENT_TYPE_FIELD):
-        if field not in header_row:
-          raise CheckException('CSV header %s not found in component csv.' % field)
-      id_idx = header_row.index(COMPONENT_ID_FIELD)
-      type_idx = header_row.index(COMPONENT_TYPE_FIELD)
+            # mandatory fields
+            for field in (COMPONENT_ID_FIELD, COMPONENT_NAME_FIELD, COMPONENT_TYPE_FIELD):
+                if field not in header_row:
+                    raise CheckException('CSV header %s not found in component csv.' % field)
+            id_idx = header_row.index(COMPONENT_ID_FIELD)
+            type_idx = header_row.index(COMPONENT_TYPE_FIELD)
 
-      for row in reader:
-        data = dict(zip(header_row, row))
-        data['labels'] = instance_tags
-        self.component(instance_key=instance_key, id=row[id_idx], type={"name": row[type_idx]}, data=data)
+            for row in reader:
+                data = dict(zip(header_row, row))
+                data['labels'] = instance_tags
+                self.component(instance_key=instance_key, id=row[id_idx], type={"name": row[type_idx]}, data=data)
 
-  def handle_relation_csv(self, instance_key, filelocation, delimiter, instance_tags):
-    self.log.debug("Processing relation CSV file %s." % filelocation)
+    def handle_relation_csv(self, instance_key, filelocation, delimiter, instance_tags):
+        self.log.debug("Processing relation CSV file %s." % filelocation)
 
-    RELATION_SOURCE_ID_FIELD = 'sourceid'
-    RELATION_TARGET_ID_FIELD = 'targetid'
-    RELATION_TYPE_FIELD = 'type'
+        RELATION_SOURCE_ID_FIELD = 'sourceid'
+        RELATION_TARGET_ID_FIELD = 'targetid'
+        RELATION_TYPE_FIELD = 'type'
 
-    with codecs.open(filelocation, mode='r', encoding="utf-8-sig") as csvfile:
-      reader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
+        with codecs.open(filelocation, mode='r', encoding="utf-8-sig") as csvfile:
+            reader = csv.reader(csvfile, delimiter=delimiter, quotechar='|')
 
-      header_row = next(reader, None)
-      if header_row is None:
-        raise CheckException("Relation CSV file is empty.")
-      self.log.debug("Detected relation header: %s" % str(header_row))
+            header_row = next(reader, None)
+            if header_row is None:
+                raise CheckException("Relation CSV file is empty.")
+            self.log.debug("Detected relation header: %s" % str(header_row))
 
-      # mandatory fields
-      for field in (RELATION_SOURCE_ID_FIELD, RELATION_TARGET_ID_FIELD, RELATION_TYPE_FIELD):
-        if field not in header_row:
-          raise CheckException('CSV header %s not found in relation csv.' % field)
-      source_id_idx = header_row.index(RELATION_SOURCE_ID_FIELD)
-      target_id_idx = header_row.index(RELATION_TARGET_ID_FIELD)
-      type_idx = header_row.index(RELATION_TYPE_FIELD)
+            # mandatory fields
+            for field in (RELATION_SOURCE_ID_FIELD, RELATION_TARGET_ID_FIELD, RELATION_TYPE_FIELD):
+                if field not in header_row:
+                    raise CheckException('CSV header %s not found in relation csv.' % field)
+            source_id_idx = header_row.index(RELATION_SOURCE_ID_FIELD)
+            target_id_idx = header_row.index(RELATION_TARGET_ID_FIELD)
+            type_idx = header_row.index(RELATION_TYPE_FIELD)
 
-      for row in reader:
-        data = dict(zip(header_row, row))
-        data['labels'] = instance_tags
-        self.relation(instance_key=instance_key,
-                      source_id=row[source_id_idx],
-                      target_id=row[target_id_idx],
-                      type={"name": row[type_idx]},
-                      data=data)
-
+            for row in reader:
+                data = dict(zip(header_row, row))
+                data['labels'] = instance_tags
+                self.relation(instance_key=instance_key,
+                              source_id=row[source_id_idx],
+                              target_id=row[target_id_idx],
+                              type={"name": row[type_idx]},
+                              data=data)
--- static_topology/test_static_topology.py
+++ static_topology/test_static_topology.py
@@ -2,271 +2,269 @@
 import os
 
 # 3p
-from mock import Mock, MagicMock
-from pyVmomi import vim  # pylint: disable=E0611
-import simplejson as json
 import mock
 
 # datadog
-from tests.checks.common import AgentCheckTest, Fixtures
+from tests.checks.common import AgentCheckTest
 from checks import CheckException
 
 FIXTURE_DIR = os.path.join(os.path.dirname(__file__), 'ci')
 
 
 class MockFileReader:
-  """used to mock codec.open, it returns content for a with-construct."""
-  def __init__(self, location, content):
-    """
-    :param location: the csv file location, used to return value of content[location]
-    :param content: dict(key: location, value: array of to delimit strings)
-    """
-    self.content = content
-    self.location = location
+    """used to mock codec.open, it returns content for a with-construct."""
 
-  def __enter__(self):
-    return self.content[self.location]
+    def __init__(self, location, content):
+        """
+        :param location: the csv file location, used to return value of content[location]
+        :param content: dict(key: location, value: array of to delimit strings)
+        """
+        self.content = content
+        self.location = location
 
-  def __exit__(self, type, value, traceback):
-    pass
+    def __enter__(self):
+        return self.content[self.location]
+
+    def __exit__(self, type, value, traceback):
+        pass
 
 
 class TestStaticCSVTopology(AgentCheckTest):
-  """
-  Unit tests for Static Topology AgentCheck.
-  """
-  CHECK_NAME = "static_topology"
+    """
+    Unit tests for Static Topology AgentCheck.
+    """
+    CHECK_NAME = "static_topology"
 
-  config = {
-    'init_config': {},
-    'instances': [
-      {
-        'type': 'csv',
-        'components_file': '/dev/null',
-        'relations_file': '/dev/null',
-        'delimiter': ';'
-      }
-    ]
-  }
-
-  def test_omitted_component_file(self):
     config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'delimiter': ';'
-        }
-      ]
-    }
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-      self.assertTrue('Static topology instance missing "components_file" value.' in context.exception)
-
-  def test_empty_component_file(self):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': '/dev/null',
-          'relations_file': '/dev/null',
-          'delimiter': ';'
-        }
-      ]
-    }
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-    self.assertEquals('Component CSV file is empty.', str(context.exception))
-
-  def test_empty_relation_file(self):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': '/ignored/',
-          'relations_file': '/dev/null',
-          'delimiter': ';'
-        }
-      ]
-    }
-
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config, mocks={
-        'handle_component_csv': lambda instance_key,filelocation,delimiter,instance_tags: None
-      })
-
-    self.assertEquals('Relation CSV file is empty.', str(context.exception))
-
-  @mock.patch('codecs.open',
-              side_effect=lambda location,mode,encoding: MockFileReader(location, {
-                'component.csv': ['id,name,type', '1,name1,type1', '2,name2,type2'],
-                'relation.csv': ['sourceid,targetid,type', '1,2,type']}))
-  def test_topology(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'relations_file': 'relation.csv',
-          'delimiter': ','
-        }
-      ]
-    }
-    self.run_check(config)
-    instances = self.check.get_topology_instances()
-    self.assertEqual(len(instances), 1)
-    self.assertEqual(len(instances[0]['components']), 2)
-    self.assertEqual(len(instances[0]['relations']), 1)
-
-  @mock.patch('codecs.open',
-              side_effect=lambda location, mode, encoding: MockFileReader(location, {
-                'component.csv': ['id,name,type', '1,name1,type1', '2,name2,type2'],
-                'relation.csv': ['sourceid,targetid,type', '1,2,type']}))
-  def test_topology_with_instance_tags(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'relations_file': 'relation.csv',
-          'delimiter': ',',
-          'tags': ['tag1', 'tag2']
-        }
-      ]
-    }
-    self.run_check(config)
-    instances = self.check.get_topology_instances()
-    self.assertEqual(len(instances), 1)
-    self.assertEqual(len(instances[0]['components']), 2)
-    self.assertEqual(len(instances[0]['components'][0]['data']['labels']), 2)
-    self.assertEqual(len(instances[0]['components'][1]['data']['labels']), 2)
-
-    self.assertEqual(len(instances[0]['relations']), 1)
-    self.assertEqual(len(instances[0]['relations'][0]['data']['labels']), 2)
-
-  @mock.patch('codecs.open', side_effect = lambda location,mode,encoding: MockFileReader(location, {
-                'component.csv': ['NOID,name,type'],
-                'relation.csv': []}))
-  def test_missing_component_id_field(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'delimiter': ','
-        }
-      ]
-    }
-
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-
-    self.assertEquals('CSV header id not found in component csv.', str(context.exception))
-
-  @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
-    'component.csv': ['id,NONAME,type'],
-    'relation.csv': []}))
-  def test_missing_component_name_field(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'delimiter': ','
-        }
-      ]
-    }
-
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-
-    self.assertEquals('CSV header name not found in component csv.', str(context.exception))
-
-  @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
-    'component.csv': ['id,name,NOTYPE'],
-    'relation.csv': []}))
-  def test_missing_component_id_field(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'delimiter': ','
-        }
-      ]
-    }
-
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-
-    self.assertEquals('CSV header type not found in component csv.', str(context.exception))
-
-  @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
-    'component.csv': ['id,name,type'],
-    'relation.csv': ['NOSOURCEID,targetid,type']}))
-  def test_missing_relation_sourceid_field(self, mock):
-    config = {
-      'init_config': {},
-      'instances': [
-        {
-          'type': 'csv',
-          'components_file': 'component.csv',
-          'relations_file': 'relation.csv',
-          'delimiter': ','
-        }
-      ]
-    }
-
-    with self.assertRaises(CheckException) as context:
-      self.run_check(config)
-
-    self.assertEquals('CSV header sourceid not found in relation csv.', str(context.exception))
-
-    @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
-      'component.csv': ['id,name,type'],
-      'relation.csv': ['sourceid,NOTARGETID,type']}))
-    def test_missing_relation_targetid_field(self, mock):
-      config = {
         'init_config': {},
         'instances': [
-          {
-            'type': 'csv',
-            'components_file': 'component.csv',
-            'relations_file': 'relation.csv',
-            'delimiter': ','
-          }
+            {
+                'type': 'csv',
+                'components_file': '/dev/null',
+                'relations_file': '/dev/null',
+                'delimiter': ';'
+            }
         ]
-      }
+    }
 
-      with self.assertRaises(CheckException) as context:
+    def test_omitted_component_file(self):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'delimiter': ';'
+                }
+            ]
+        }
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
+            self.assertTrue('Static topology instance missing "components_file" value.' in context.exception)
+
+    def test_empty_component_file(self):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': '/dev/null',
+                    'relations_file': '/dev/null',
+                    'delimiter': ';'
+                }
+            ]
+        }
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
+        self.assertEquals('Component CSV file is empty.', str(context.exception))
+
+    def test_empty_relation_file(self):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': '/ignored/',
+                    'relations_file': '/dev/null',
+                    'delimiter': ';'
+                }
+            ]
+        }
+
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config, mocks={
+                'handle_component_csv': lambda instance_key, filelocation, delimiter, instance_tags: None
+            })
+
+        self.assertEquals('Relation CSV file is empty.', str(context.exception))
+
+    @mock.patch('codecs.open',
+                side_effect=lambda location, mode, encoding: MockFileReader(location, {
+                    'component.csv': ['id,name,type', '1,name1,type1', '2,name2,type2'],
+                    'relation.csv': ['sourceid,targetid,type', '1,2,type']}))
+    def test_topology(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'relations_file': 'relation.csv',
+                    'delimiter': ','
+                }
+            ]
+        }
         self.run_check(config)
+        instances = self.check.get_topology_instances()
+        self.assertEqual(len(instances), 1)
+        self.assertEqual(len(instances[0]['components']), 2)
+        self.assertEqual(len(instances[0]['relations']), 1)
 
-      self.assertEquals('CSV header targetid not found in relation csv.', str(context.exception))
+    @mock.patch('codecs.open',
+                side_effect=lambda location, mode, encoding: MockFileReader(location, {
+                    'component.csv': ['id,name,type', '1,name1,type1', '2,name2,type2'],
+                    'relation.csv': ['sourceid,targetid,type', '1,2,type']}))
+    def test_topology_with_instance_tags(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'relations_file': 'relation.csv',
+                    'delimiter': ',',
+                    'tags': ['tag1', 'tag2']
+                }
+            ]
+        }
+        self.run_check(config)
+        instances = self.check.get_topology_instances()
+        self.assertEqual(len(instances), 1)
+        self.assertEqual(len(instances[0]['components']), 2)
+        self.assertEqual(len(instances[0]['components'][0]['data']['labels']), 2)
+        self.assertEqual(len(instances[0]['components'][1]['data']['labels']), 2)
+
+        self.assertEqual(len(instances[0]['relations']), 1)
+        self.assertEqual(len(instances[0]['relations'][0]['data']['labels']), 2)
 
     @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
-      'component.csv': ['id,name,type'],
-      'relation.csv': ['sourceid,targetid,NOTYPE']}))
-    def test_missing_relation_type_field(self, mock):
-      config = {
-        'init_config': {},
-        'instances': [
-          {
-            'type': 'csv',
-            'components_file': 'component.csv',
-            'relations_file': 'relation.csv',
-            'delimiter': ','
-          }
-        ]
-      }
+        'component.csv': ['NOID,name,type'],
+        'relation.csv': []}))
+    def test_missing_component_id_field(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'delimiter': ','
+                }
+            ]
+        }
 
-      with self.assertRaises(CheckException) as context:
-        self.run_check(config)
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
 
-      self.assertEquals('CSV header type not found in relation csv.', str(context.exception))
+        self.assertEquals('CSV header id not found in component csv.', str(context.exception))
+
+    @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
+        'component.csv': ['id,NONAME,type'],
+        'relation.csv': []}))
+    def test_missing_component_name_field(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'delimiter': ','
+                }
+            ]
+        }
+
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
+
+        self.assertEquals('CSV header name not found in component csv.', str(context.exception))
+
+    @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
+        'component.csv': ['id,name,NOTYPE'],
+        'relation.csv': []}))
+    def test_missing_component_id_field(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'delimiter': ','
+                }
+            ]
+        }
+
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
+
+        self.assertEquals('CSV header type not found in component csv.', str(context.exception))
+
+    @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
+        'component.csv': ['id,name,type'],
+        'relation.csv': ['NOSOURCEID,targetid,type']}))
+    def test_missing_relation_sourceid_field(self, mock):
+        config = {
+            'init_config': {},
+            'instances': [
+                {
+                    'type': 'csv',
+                    'components_file': 'component.csv',
+                    'relations_file': 'relation.csv',
+                    'delimiter': ','
+                }
+            ]
+        }
+
+        with self.assertRaises(CheckException) as context:
+            self.run_check(config)
+
+        self.assertEquals('CSV header sourceid not found in relation csv.', str(context.exception))
+
+        @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
+            'component.csv': ['id,name,type'],
+            'relation.csv': ['sourceid,NOTARGETID,type']}))
+        def test_missing_relation_targetid_field(self, mock):
+            config = {
+                'init_config': {},
+                'instances': [
+                    {
+                        'type': 'csv',
+                        'components_file': 'component.csv',
+                        'relations_file': 'relation.csv',
+                        'delimiter': ','
+                    }
+                ]
+            }
+
+            with self.assertRaises(CheckException) as context:
+                self.run_check(config)
+
+            self.assertEquals('CSV header targetid not found in relation csv.', str(context.exception))
+
+        @mock.patch('codecs.open', side_effect=lambda location, mode, encoding: MockFileReader(location, {
+            'component.csv': ['id,name,type'],
+            'relation.csv': ['sourceid,targetid,NOTYPE']}))
+        def test_missing_relation_type_field(self, mock):
+            config = {
+                'init_config': {},
+                'instances': [
+                    {
+                        'type': 'csv',
+                        'components_file': 'component.csv',
+                        'relations_file': 'relation.csv',
+                        'delimiter': ','
+                    }
+                ]
+            }
+
+            with self.assertRaises(CheckException) as context:
+                self.run_check(config)
+
+            self.assertEquals('CSV header type not found in relation csv.', str(context.exception))

0aa406e17f42326fde5785d64357370d20b20d73
Wytze Hazenberg <whazenberg@stackstate.com>
2019-05-15 13:52:33 +0200

    Google Analytics iteration + unit tests

--- google_analytics/check.py
+++ google_analytics/check.py
@@ -1,17 +1,6 @@
 """
 Google Analytics check
 Collects metrics from the Analytics API.
-Jonathan Makuc - Bithaus Chile (Datadog Partner) - jmakuc@bithaus.cl
-
-2016-04-13
-- Support for pageViews metric
-- Metric value is read from "1 minute ago" instead of "during the last minute"
-  in order to obtain a consistent value to report to Datadog. Using "during the last 
-  minute" result in reading zeros while waiting for visitors to view pages in that time 
-  frame.
-- Dimensions and tags can be controlled on yaml file
-
-
 """
 
 # the following try/except block will make the custom check compatible with any Agent version
@@ -27,98 +16,183 @@ import googleapiclient.discovery
 
 
 class GoogleAnalyticsCheck(AgentCheck):
-  """ Collects as many metrics as instances defined in ga.yaml
-  """
+    """
+    Collects metrics based on what is defined in the yaml
+    """
 
-  scope = ['https://www.googleapis.com/auth/analytics.readonly']
-  service = 0
-  apiName = 'analytics'
-  version = 'v3'
+    scope = ['https://www.googleapis.com/auth/analytics.readonly']
+    service = None
+    apiName = 'analytics'
+    version = 'v3'
 
-  def check(self, instance):
-    self.log.info('profile: %s, tags: %s, pageview_dimensions: %s' % (instance.get('profile'), instance.get('tags'), instance.get('pageview_dimensions')))        
-    
-    profile = instance.get('profile')
-    instanceTags = instance.get('tags')
-    instanceTags.append("profile:" + profile)
+    def check(self, instance):
+        if not self.service:
+            self.service = self.get_ga_service(
+                self.apiName,
+                self.version,
+                self.scope,
+                self.init_config.get('key_file_location'))
 
-    # pageview collection
-    metricName = 'rt:pageviews'
-    pageviewsDims = ['rt:minutesAgo']
-    confDims = instance.get('pageview_dimensions');
-    if isinstance(confDims, list):
-      pageviewsDims = pageviewsDims + confDims
-    result = self.get_results(profile, metricName, pageviewsDims)
-    self.log.debug(result)
-    headers = result.get('columnHeaders')
-    rows = result.get('rows', [])
+        profile = instance.get('profile')
+        instance_tags = instance.get('tags', [])
+        instance_tags.append("profile:" + profile)
+        metrics = instance.get('metrics', [])
+        dimensions = instance.get('dimensions', [])
+        filters = instance.get('filters', [])
 
-    if len(rows) < 1:
-      return
-      
-    pvMetricsSent = 0
+        is_realtime = reduce((lambda is_rt, metric: is_rt or metric[:3] == "rt:"), metrics, False)
+        self.log.debug("Use of real time API: %s" % is_realtime)
 
-    for row in rows:
-      # In order to have a consistent metric, we look for the value 1 minute ago
-      # and not during the last minute.
-      if int(row[0]) == 1:
-        tags = []
-        tags.extend(instanceTags)
-        for i in xrange(len(headers)-1):
-          if i > 0:
-            # we remove the "rt" from the dimension name
-            tags.append(headers[i].get('name')[3:] + ":" + row[i])
-            
-        self.gauge("googleanalytics.rt.pageviews", 
-          row[len(row)-1], 
-          tags=tags,
-          hostname=None,
-          device_name=None)
-        
-        pvMetricsSent = pvMetricsSent + 1
-        
-    self.log.info("Pageview Metrics sent %s" % pvMetricsSent)
+        if is_realtime:
+            self.log.debug('profile: %s, real-time, metric: %s, dimensions: %s' % (profile, metrics, dimensions))
+            self.process_realtime(profile, instance_tags, metrics, dimensions, filters)
+        else:  # non- real time queries
+            start_time = instance.get('start_time')
+            end_time = instance.get('end_time')
+            self.log.debug('profile: %s, metrics: %s, dimensions: %s, start_time: %s, end_time:%s' % (profile, metrics, dimensions, start_time, end_time))
+            self.process_ga(profile, instance_tags, metrics, dimensions, filters, start_time, end_time)
 
-    # activeUsers collection
-    metricName = 'rt:activeUsers'
-    activeuserDims = []
-    tags = []
-    tags.extend(instanceTags)
+    def process_realtime(self, profile, instance_tags, metrics, dimensions, filters):
+        """
+        Process real time Google Analytics API request
+        :param profile: Google Analytics Profile ID
+        :param instance_tags: tags to add to the retrieved metrics as set in the yaml
+        :param metrics: the selected Google Analytics metrics as specified in the yaml
+        :param dimensions: the selected Google Analytics dimensions as specified in the yaml
+        :param filters: the selected Google Analytics filters as specified in the yaml
+        :return: nothing
+        """
+        result = self.get_rt_results(profile, metrics, dimensions, filters)
+        self.log.debug("Real time response: %s" % result)
 
-    result = self.get_results(profile, metricName, activeuserDims)
+        if "rt:minutesAgo" in dimensions:
+            header_names = [header.get('name') for header in result.get('columnHeaders')]
+            minutes_ago_idx = header_names.index("rt:minutesAgo")
 
-    activeUsers = int(result.get("totalsForAllResults").get(metricName))
+            # In order to have a consistent metric, we look for the value of one minute ago and not during the last minute.
+            rows = result.get('rows', [])
+            filtered_rows = filter(lambda row: int(row[minutes_ago_idx]) == 1, rows)
+            result['rows'] = filtered_rows
 
-    self.gauge("googleanalytics.rt.activeUsers",
-               activeUsers,
-               tags=tags,
-               hostname=None,
-               device_name=None)
+        self.process_response(instance_tags, result)
 
-    self.log.info("Active users %s" % activeUsers);
+    def process_ga(self, profile, instance_tags, metrics, dimensions, filters, start_time, end_time):
+        """
+        Process non- real time Google Analytics API request
+        :param profile: Google Analytics Profile ID
+        :param instance_tags: tags to add to the retrieved metrics as set in the yaml
+        :param metrics: the selected Google Analytics metrics as specified in the yaml
+        :param dimensions: the selected Google Analytics dimensions as specified in the yaml
+        :param filters: the selected Google Analytics filters as specified in the yaml
+        :param start_time: the selected Google Analytics start time of the query as specified in the yaml
+        :param end_time: the selected Google Analytics end time of the query as specified in the yaml
+        :return: nothing
+        """
 
-  def __init__(self, *args, **kwargs):
-    AgentCheck.__init__(self, *args, **kwargs)
-    self.log.info('key_file_location: %s' % self.init_config.get('key_file_location'))
-    
-    self.service = self.get_service(
-      self.apiName, 
-      self.version, 
-      self.scope, 
-      self.init_config.get('key_file_location'))
+        result = self.get_ga_results(profile, metrics, dimensions, filters, start_time, end_time)
+        self.log.debug("GA response: %s" % result)
+        self.process_response(instance_tags, result)
 
-  def get_service(self, api_name, api_version, scope, key_file_location):
-    credentials = service_account.Credentials.from_service_account_file(key_file_location, scopes=scope)
-    service = googleapiclient.discovery.build(api_name, api_version, credentials=credentials)
-    return service
+    def process_response(self, instance_tags, result):
+        """
+        Process Google Analytics (real time) API responses and emits metrics.
+        :param instance_tags: tags to add to the retrieved metrics as set in the yaml
+        :param result: the API response in json
+        :return: nothing
+        """
+        def replace(string):
+            """
+            replaces the following prefixes for metric key and tag compatibility
+            - 'ga:' to 'ga.'
+            - 'rt:' to 'rt.'
+            :param string: input
+            :return: transformed input
+            """
+            if string[:3] == "ga:" or string[:3] == "rt:":
+                return string.replace(':', '.', 1)
+            return string
 
-  def get_results(self, profile_id, the_metric, dims):
-    if len(dims) > 0:
-      return self.service.data().realtime().get(
-        ids=profile_id,
-        metrics=the_metric,
-        dimensions=','.join(dims)).execute()
-    else:
-      return self.service.data().realtime().get(
-        ids=profile_id,
-        metrics=the_metric).execute()
+        # extract the location of the dimensions and metrics
+        headers = result.get('columnHeaders')
+        dimensions = []
+        dimension_indices = []
+        metrics = []
+        metric_indices = []
+        for header_idx in xrange(len(headers)):
+            header = headers[header_idx]
+            header_name = header.get('name')
+            header_column_type = header.get("columnType")
+            if header_column_type == "DIMENSION":
+                dimensions += [replace(header_name)]
+                dimension_indices += [header_idx]
+            elif header_column_type == "METRIC":
+                metrics += [replace(header_name)]
+                metric_indices += [header_idx]
+
+        # iterate over the resulting results, rows are not always present
+        rows = result.get('rows', [])
+        for row in rows:
+            extracted_dimensions = [row[dimension_idx] for dimension_idx in dimension_indices]
+            extracted_metrics = [row[dimension_idx] for dimension_idx in metric_indices]
+            dimensions_with_keys = zip(dimensions, extracted_dimensions)
+            metrics_with_keys = zip(metrics, extracted_metrics)
+
+            tags = []
+            tags += instance_tags
+            tags += map(lambda (k, v): "%s:%s" % (k, v), dimensions_with_keys)
+
+            for metric_key, metric_value in metrics_with_keys:
+                self.gauge("googleanalytics.%s" % metric_key, int(metric_value), tags=tags)
+
+    def get_ga_service(self, api_name, api_version, scope, key_file_location):
+        """
+        Get Google Analytics API object
+        :param api_name: Google Analytics API name to use
+        :param api_version: Google Analytics API version to use
+        :param scope: Google Analytics API scope to use
+        :param key_file_location: the file location of the Google credentials JSON
+        :return: Google Resource for interacting with the service.
+        """
+        credentials = service_account.Credentials.from_service_account_file(key_file_location, scopes=scope)
+        service = googleapiclient.discovery.build(api_name, api_version, credentials=credentials)
+        return service
+
+    def get_rt_results(self, profile_id, metrics, dimensions=None, filters=None):
+        """
+        Obtain real time results from Google Analytics API
+        :param profile_id: Google Analytics Profile ID
+        :param metrics: list, the selected Google Analytics metrics as specified in the yaml
+        :param dimensions: list, the selected Google Analytics dimensions as specified in the yaml
+        :param filters: the selected Google Analytics filters as specified in the yaml
+        :return: json
+        """
+        filters = ';'.join(filters) if filters else None  # Default filter is AND, comma is OR
+        dimensions = ','.join(dimensions) if dimensions else None
+        return self.service.data().realtime().get(
+            ids=profile_id,
+            metrics=','.join(metrics),
+            dimensions=dimensions,
+            filters=filters
+        ).execute()
+
+    def get_ga_results(self, profile_id, metrics, dimensions, filters, start_time, end_time):
+        """
+        Obtain non- real time results from Google Analytics API
+        :param profile_id: Google Analytics Profile ID
+        :param metrics: list, the selected Google Analytics metrics as specified in the yaml
+        :param dimensions: list, the selected Google Analytics dimensions as specified in the yaml
+        :param filters: the selected Google Analytics filters as specified in the yaml
+        :param start_time: the selected Google Analytics start time of the query as specified in the yaml
+        :param end_time: the selected Google Analytics end time of the query as specified in the yaml
+        :return: json
+        """
+        filters = ';'.join(filters) if filters else None # Default filter is AND, comma is OR
+        dimensions = ','.join(dimensions) if dimensions else None
+        return self.service.data().ga().get(
+            ids=profile_id,
+            start_date=start_time,
+            end_date=end_time,
+            metrics=','.join(metrics),
+            dimensions=dimensions,
+            filters=filters
+        ).execute()
---
+++ google_analytics/test_google_analytics.py
@@ -0,0 +1,264 @@
+# stdlib
+import os
+
+# 3p
+import json
+from mock import Mock
+
+# datadog
+from tests.checks.common import AgentCheckTest, Fixtures
+
+FIXTURE_DIR = os.path.join(os.path.dirname(__file__), 'ci')
+
+
+class GoogleAnalytics(AgentCheckTest):
+    CHECK_NAME = "google_analytics"
+
+    @staticmethod
+    def _config(instances):
+        def include_profile(instance):
+            instance['profile'] = 'ga:12345678'
+            return instance
+
+        return {
+            'init_config': {
+                'key_file_location': '/dev/null'
+            },
+            'instances': map(lambda instance: include_profile(instance), instances)
+        }
+
+    @staticmethod
+    def _get_json(file_name):
+        return json.loads(Fixtures.read_file("%s.json" % file_name, sdk_dir=FIXTURE_DIR))
+
+
+class TestRealtimeGoogleAnalytics(GoogleAnalytics):
+    """
+    Unit tests for Real time Google Analytics AgentCheck.
+    """
+
+    def test_detect_real_time_required(self):
+        process_realtime_mock = Mock()
+        process_ga_mock = Mock()
+
+        self.run_check(self._config([{
+            'metrics': ['rt:pageviews']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'process_realtime': process_realtime_mock,
+            'process_ga': process_ga_mock
+        })
+
+        self.assertTrue(process_realtime_mock.called, msg='Method process_realtime should not have been called.')
+        self.assertFalse(process_ga_mock.called, msg='Method process_ga should not have been called.')
+
+    def test_detect_non_real_time_required(self):
+        process_realtime_mock = Mock()
+        process_ga_mock = Mock()
+
+        self.run_check(self._config([{
+            'metrics': ['ga:pageviews']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'process_realtime': process_realtime_mock,
+            'process_ga': process_ga_mock
+        })
+
+        self.assertFalse(process_realtime_mock.called, msg='Method process_realtime should not have been called.')
+        self.assertTrue(process_ga_mock.called, msg='Method process_ga should not have been called.')
+
+    def test_metric(self):
+        self.run_check(self._config([{
+            'metrics': ['rt:pageviews'],
+            'dimensions': ['rt:minutesAgo']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_one_dimension_no_filter"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=40, tags=['profile:ga:12345678', 'rt.minutesAgo:01'], count=1)
+
+    def test_one_metric_one_dimension_minutesAgo_no_filter_no_minute(self):
+        self.run_check(self._config([{
+            'is_realtime': True,
+            'metrics': ['rt:pageviews'],
+            'dimensions': ['rt:minutesAgo']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_one_dimension_no_filter_no_minute_value"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertEqual(len(self.metrics), 0, "No metrics should have been collected.")
+
+    def test_one_metric_empty_results(self):
+        self.run_check(self._config([{
+            'is_realtime': True,
+            'metrics': ['rt:pageviews'],
+            'dimensions': ['rt:minutesAgo']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_one_dimension_no_filter_no_result"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertEqual(len(self.metrics), 0, "No metrics should have been collected.")
+
+    def test_one_metric_no_dimension_no_filter(self):
+        self.run_check(self._config([{
+            'is_realtime': True,
+            'metrics': ['rt:activeUsers'],
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_no_dimensions_no_filter"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertMetric(metric_name="googleanalytics.rt.activeUsers", value=2, tags=['profile:ga:12345678'], count=1)
+        self.assertEqual(len(self.metrics), 1, msg='One metric should have been collected.')
+
+    def test_one_metric_two_dimensions_one_filter(self):
+        self.run_check(self._config([{
+            'is_realtime': True,
+            'metrics': ['rt:pageviews'],
+            'dimensions': ['rt:minutesAgo', 'rt:pagePath'],
+            'filters': 'rt:pagePath=~^/booker/selection.outbound'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_two_dimensions_one_filter"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=1, tags=['profile:ga:12345678', 'rt.minutesAgo:01', 'rt.pagePath:/booker/selection.outbound'], count=1)
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=2, tags=['profile:ga:12345678', 'rt.minutesAgo:01', 'rt.pagePath:/booker/selection.outbound.connections'], count=1)
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=2, tags=['profile:ga:12345678', 'rt.minutesAgo:01', 'rt.pagePath:/booker/selection.outbound.connections/bookingSummaryModal'], count=1)
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=1, tags=['profile:ga:12345678', 'rt.minutesAgo:01', 'rt.pagePath:/booker/selection.outbound.detail'], count=1)
+        self.assertEqual(len(self.metrics), 4, msg='Four metrics should have been collected.')
+
+    def test_metric_instance_tags(self):
+        self.run_check(self._config([{
+            'is_realtime': True,
+            'metrics': ['rt:pageviews'],
+            'dimensions': ['rt:minutesAgo'],
+            'tags': ['env:test', 'key:value']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: self._get_json("realtime_one_metric_one_dimension_no_filter"),
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: None
+        })
+        self.assertMetric(metric_name="googleanalytics.rt.pageviews", value=40, tags=['profile:ga:12345678', 'rt.minutesAgo:01', 'env:test', 'key:value'], count=1)
+
+
+class TestGoogleAnalytics(GoogleAnalytics):
+    """
+    Unit tests for non- real time Google Analytics AgentCheck.
+    """
+
+    def test_empty_metrics(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews', 'ga:users'],
+            'dimensions': ['ga:pagePath', 'ga:browser'],
+            'filters': ['ga:pagePath==/booker_v3/confirmation', 'ga:browser==Chrome2'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_two_metrics_two_dimensions_two_filters_no_results")
+        })
+        self.assertEqual(len(self.metrics), 0, msg='No metrics should have been collected.')
+
+    def test_empty_dimensions(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_one_metric_no_dimensions_no_filters")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=8119, tags=['profile:ga:12345678'], count=1)
+        self.assertEqual(len(self.metrics), 1, msg='One metric should have been collected.')
+
+    def test_one_metric_one_dimension_one_filter(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews'],
+            'dimensions': ['ga:pagePath'],
+            'filters': 'ga:pagePath==/booker_v3/confirmation',
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_one_metric_one_dimension_one_filter")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=11, tags=['profile:ga:12345678','ga.pagePath:/booker/confirmation'], count=1)
+        self.assertEqual(len(self.metrics), 1, msg='One metric should have been collected.')
+
+    def test_one_metric_one_dimension_no_filter(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews'],
+            'dimensions': ['ga:pagePath'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_one_metric_one_dimension_no_filter")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=29, tags=['profile:ga:12345678', 'ga.pagePath:/'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=39, tags=['profile:ga:12345678', 'ga.pagePath:/aftersales/cancel-step-1'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=15, tags=['profile:ga:12345678', 'ga.pagePath:/aftersales/cancel-step-2'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=0, tags=['profile:ga:12345678', 'ga.pagePath:/nl/tickets'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=0, tags=['profile:ga:12345678', 'ga.pagePath:/nl/tickets-v3/'], count=1)
+        self.assertEqual(len(self.metrics), 5, msg='Five metrics should have been collected.')
+
+    def test_two_metrics_one_dimension_one_filter(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews', 'ga:users'],
+            'dimensions': ['ga:pagePath'],
+            'filters': ['ga:pagePath==/booker_v3/confirmation'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_two_metrics_one_dimension_one_filter")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=11, tags=['profile:ga:12345678', 'ga.pagePath:/booker/confirmation'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.users", value=6, tags=['profile:ga:12345678', 'ga.pagePath:/booker/confirmation'], count=1)
+        self.assertEqual(len(self.metrics), 2, msg='Two metrics should have been collected.')
+
+    def test_two_metrics_two_dimensions_two_filters(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews', 'ga:users'],
+            'dimensions': ['ga:pagePath', 'ga:browser'],
+            'filters': ['ga:pagePath==/booker_v3/confirmation', 'ga:browser==Chrome'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo'
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_two_metrics_two_dimensions_two_filters")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=11, tags=['profile:ga:12345678', 'ga.pagePath:/booker/confirmation', 'ga.browser:Chrome'], count=1)
+        self.assertMetric(metric_name="googleanalytics.ga.users", value=6, tags=['profile:ga:12345678', 'ga.pagePath:/booker/confirmation', 'ga.browser:Chrome'], count=1)
+        self.assertEqual(len(self.metrics), 2, msg='Two metrics should have been collected.')
+
+    def test_instance_tags(self):
+        self.run_check(self._config([{
+            'is_realtime': False,
+            'metrics': ['ga:pageviews'],
+            'start_time': '2daysAgo',
+            'end_time': '1daysAgo',
+            'tags': ['tag:tag1', 'key:value']
+        }]), mocks={
+            'get_ga_service': lambda api_name, api_version, scope, key_file_location: None,
+            'get_rt_results': lambda profile_id, metric, dimensions, filters: None,
+            'get_ga_results': lambda profile_id, metrics, dimensions, filters, start_time, end_time: self._get_json("ga_one_metric_no_dimensions_no_filters")
+        })
+        self.assertMetric(metric_name="googleanalytics.ga.pageviews", value=8119, tags=['profile:ga:12345678','tag:tag1', 'key:value'], count=1)
+        self.assertEqual(len(self.metrics), 1, msg='One metric should have been collected.')
